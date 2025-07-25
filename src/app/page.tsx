452654188944c8119d6418ced2d4ba60e70ef19a
sujitIwale <sujit.iwale5115@gmail.com>
2025-07-25 13:18:49 +0530

    feat: adding missing project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import styles from "./page.module.css";
 import ProjectCard from "@/components/ProjectCard";
 import HeroSection from "./HeroSection";
-import ExperienceSection from "./ExperienceSection";
 import Typography from "@/components/common/Typography";
 import Skills from "@/components/sections/Skills/Skills";
 import { projects } from "@/data/data";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
     <main className={styles["page-container"]}>
       <HeroSection />
       <Skills />
-      <ExperienceSection />
+      <Experience />
       <section className={styles["projects-section"]} id="projects">
         <Typography variant="h2">My Projects</Typography>
         <div className={styles["projects-list"]}>
